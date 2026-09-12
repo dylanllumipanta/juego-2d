5d52c88c218a925a2f4a8b4ca639271bdcdc4f99
dylanllumipanta <dylanlumipanta2011@gmail.com>
2026-09-12 16:29:17 -0500

    animaciones listas

--- PlayerControler.cs
+++ PlayerControler.cs
@@ -3,27 +3,47 @@ using UnityEngine;
 public class PlayerControler : MonoBehaviour
 {
     private Rigidbody2D rd;
+    private Animator animator;
 
     public float speed = 5f;
     public float jumpForce = 7f;
 
     private bool isGrounded = true;
+    private bool facingRight = true;
 
     void Start()
     {
         rd = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         float move = Input.GetAxis("Horizontal");
 
+        // Animación de caminar
+        float speedAnimation = Mathf.Abs(move);
+        animator.SetFloat("Speed", speedAnimation);
+
+        // Movimiento
         rd.linearVelocity = new Vector2(move * speed, rd.linearVelocity.y);
 
+        // Salto
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rd.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isGrounded = false;
+            animator.SetBool("IsJump", true);
+        }
+
+        // Girar personaje
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
         }
     }
 
@@ -32,6 +52,23 @@ public class PlayerControler : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+            animator.SetBool("IsJump", false);
         }
+    }
+
+    void OnCollisionExit2D(Collision2D collision){
+
+        if(collision.gameObject.CompareTag("Ground")){
+            isGrounded = false;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
